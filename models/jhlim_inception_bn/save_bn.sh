117a4c1a33c9d18240cf6842dcd4e518d4106f77
jaehyun <lim0505@gmail.com>
2015-11-12 19:44:20 +0900

    bash scripts for save batch_mean_, batch_variance_; load batch_mean_, batch_variance_

---
+++ models/jhlim_inception_bn/save_bn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CAFFE_ROOT=/home/jaehyun/github/caffe-fast-rcnn
+
+TRAIN_MODEL=models/jhlim_inception_bn/train_val.prototxt
+TEST_MODEL=models/jhlim_inception_bn/train_val.prototxt
+#WEIGHT=models/jhlim_inception_bn/inception_bn_stepsize_6400_iter_1200000.caffemodel
+WEIGHT=/media/data1/pretrained_nets/inception_bn_ilsvrc12/shuni/inception_bn_stepsize_6400_iter_1200000.caffemodel
+
+$CAFFE_ROOT/build/tools/save_bn -train_model $TRAIN_MODEL -weights $WEIGHT -train_iterations 1000 -gpu 0 
